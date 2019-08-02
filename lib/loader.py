28bb2aa83b085ae092fc0c91b41ef7ae27b714eb
bortoz <bortolin.alessandro@outlook.it>
2019-08-02 18:53:46 +0200

    Fix sigint handle

--- lib/loader.py
+++ lib/loader.py
@@ -1,10 +1,15 @@
 import os, platform, subprocess, sys, time
 
 clock = time.time()
-child = subprocess.run(sys.argv[1:])
 
-print('\nProcess returned {0} (0x{0:X})\texecution time : {1:.3f} s'.format(child.returncode, time.time() - clock))
+try:
+    child = subprocess.run(sys.argv[1:])
+    code = child.returncode
+except KeyboardInterrupt:
+    code = 2
+
+print('\nProcess returned {0} (0x{0:X})\texecution time : {1:.3f} s'.format(code, time.time() - clock))
 if platform.system() == 'Windows':
     os.system('PAUSE')
 else:
-    subprocess.run(['bash', '-c', 'read -n 1 -s -r -p \'Press any key to continue...\''])
+    subprocess.run(['bash', '-c', 'read -n 1 -s -r -p "Press any key to continue..."'])
