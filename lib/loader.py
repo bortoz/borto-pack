72b0122ef20890274878690b9aad4d0772a5bb81
bortoz <bortolin.alessandro@outlook.it>
2019-09-02 20:05:42 +0200

    Show signal code when process fail

--- lib/loader.py
+++ lib/loader.py
@@ -6,10 +6,23 @@ try:
     child = subprocess.run(sys.argv[1:])
     code = child.returncode
 except KeyboardInterrupt:
-    code = 2
+    code = -2
 
-print('\nProcess returned {0} (0x{0:X})\texecution time : {1:.3f} s'.format(code, time.time() - clock))
+if code >= 0:
+    print('\nProcess returned {0} (0x{0:X})\texecution time : {1:.3f} s'.format(code, time.time() - clock))
+if -31 <= code < 0:
+    signal = [
+        "", "SIGHUP", "SIGINT", "SIGQUIT", "SIGILL", "SIGTRAP", "SIGABRT",
+        "SIGBUS", "SIGFPE", "SIGKILL", "SIGUSR1", "SIGSEGV", "SIGUSR2",
+        "SIGPIPE", "SIGALRM", "SIGTERM", "SIGSTKFLT", "SIGCHLD", "SIGCONT",
+        "SIGSTOP", "SIGTSTP", "SIGTTIN", "SIGTTOU", "SIGURG", "SIGXCPU",
+        "SIGXFSZ", "SIGVTALRM", "SIGPROF", "SIGWINCH", "SIGIO", "SIGPWR",
+        "SIGSYS"
+    ][-code]
+    print('\nProcess terminated by signal {} ({})'.format(-code, signal))
+
 if platform.system() == 'Windows':
     os.system('PAUSE')
 else:
-    subprocess.run(['bash', '-c', 'read -n 1 -s -r -p "Press any key to continue..."'])
+    subprocess.run(
+        ['bash', '-c', 'read -n 1 -s -r -p "Press any key to continue..."'])
